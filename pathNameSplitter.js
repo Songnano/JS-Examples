0b5afb6e735a567d5c3b0094f6044f07aa91d8df
Nano <hyeonpass@naver.com>
2025-03-08 15:10:23 +0900

    Create pathNameSplitter.js

---
+++ pathNameSplitter.js
@@ -0,0 +1,8 @@
+pathName = location.pathname.split("/")[1];
+if (pathName === "directoryName" || pathName === "directoryNameSecondary") {
+  var html = "<p>This is a paragraph.</p>";
+  document.getElementsByClassName("output").insertAdjacentHTML("afterend",html);
+  // To insert the HTML into the document rather than replace the contents of an element, use the method insertAdjacentHTML()
+  // https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
+  // You use the following parameters: "afterbegin", "beforeend", "beforebegin", "afterend"
+}
