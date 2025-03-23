5a2b6ffea4fac002933509b293c494573863b4eb
Nano <hyeonpass@naver.com>
2025-03-23 19:26:23 +0900

    Create renderHTML.js

---
+++ renderHTML.js
@@ -0,0 +1,3 @@
+var toolbarHTML = "";
+toolbarHTML += "<p>" + "Everthing you want to do" + "</p>":
+document.getElementsByClassName("your-class-tag-name")[0].insertAdjacentHTML("afterend",toolbarHTML);
