3c938a4e19fc6f139cd21388988ab0f53e2c78b4
Nano <hyeonpass@naver.com>
2025-03-23 19:32:15 +0900

    Update renderHTML.js

--- renderHTML.js
+++ renderHTML.js
@@ -1,3 +1,6 @@
-var toolbarHTML = "";
-toolbarHTML += "<p>" + "Everthing you want to do" + "</p>":
-document.getElementsByClassName("your-class-tag-name")[0].insertAdjacentHTML("afterend",toolbarHTML);
+  var toolbarHTML = "";
+  toolbarHTML += "<p>First: Everything you want to do</p>" + "\n";
+  toolbarHTML += "<p>Second: Everything you want to do</p>" + "\n";
+  toolbarHTML += "<p>Third: Everything you want to do</p>" + "\n";
+  if (document.getElementsByClassName("your-class-tag-name")[0]) {
+    document.getElementsByClassName("your-class-tag-name")[0].insertAdjacentHTML("afterend", toolbarHTML);
